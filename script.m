0e0cf0576c5daa9660ea9cca73535a09c709732a
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-05-10 18:00:46 +0700

    :sparkles: Universal preprocess

--- script.m
+++ script.m
@@ -8,30 +8,22 @@ close all;
 for i = 1:7
     image = imread(sprintf('img/%d.jpg', i));
     
-    if i == 4
-        processed = preprocess4(image);
-    elseif i == 5
-        processed = preprocess5(image);
-    elseif i == 6
-        processed = preprocess6(image);
-    elseif i == 7
-        processed = preprocess7(image);
-    else
-        processed = preprocess5(image);
-    end
+    processed = preprocess(image);
     
     ocrResults = ocr(processed);
   
-    ocrResults.Text
+    ocrResults.Words
 
     position = ocrResults.WordBoundingBoxes;
     label =  ocrResults.WordConfidences;
     
     figure(i);
     if ~isempty(position)
+        image = imresize(image, 1.1);
+        image = imresize(image, 1.6);
         Iocr = insertObjectAnnotation(image, 'rectangle', position, label);
         
-        subplot(1, 2, 1), imshow(processed);
+        subplot(1, 2, 1), imshow(processed, []);
         subplot(1, 2, 2), imshow(Iocr);
     else
         imshow(processed);
