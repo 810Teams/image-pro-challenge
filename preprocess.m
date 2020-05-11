5ab061a08cbe31abe548a2b47dcbae89231d74e1
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-05-11 09:42:23 +0700

    :sparkles: More accurate blur reduction

--- preprocess.m
+++ preprocess.m
@@ -2,44 +2,28 @@
 % preprocess.m
 %
 
-function [g] = preprocess(f)
+function [g] = preprocess(f, r1, r2)
+    % -- Initialize --
     g = rgb2gray(f);
-    
-    g = imresize(g, 1.1);
+    g = imresize(g, r1);
     
     % -- Weiner --
     % https://www.mathworks.com/help/images/ref/deconvwnr.html
-    I = im2double(g);
-    PSF = fspecial('motion', 1.02, 0);
-    noise_var = 0.0001;
-    estimated_nsr = noise_var / var(I(:));
-    g = deconvwnr(g, PSF, estimated_nsr);
+    ANGLES = 12;
+    for i = 0:ANGLES - 1
+        I = im2double(g);
+        PSF = fspecial('motion', 1.015, 90 - 90/ANGLES*i);
+        noise_var = 0.0001;
+        estimated_nsr = noise_var / var(I(:));
+        g = deconvwnr(g, PSF, estimated_nsr);
+    end
     
-    I = im2double(g);
-    PSF = fspecial('motion', 1.02, 22.5);
-    noise_var = 0.0001;
-    estimated_nsr = noise_var / var(I(:));
-    g = deconvwnr(g, PSF, estimated_nsr);
+    % -- Final --
+    g = imresize(g, r2);
     
-    I = im2double(g);
-    PSF = fspecial('motion', 1.02, 45);
-    noise_var = 0.0001;
-    estimated_nsr = noise_var / var(I(:));
-    g = deconvwnr(g, PSF, estimated_nsr);
-    
-    I = im2double(g);
-    PSF = fspecial('motion', 1.02, 67.5);
-    noise_var = 0.0001;
-    estimated_nsr = noise_var / var(I(:));
-    g = deconvwnr(g, PSF, estimated_nsr);
-    
-    I = im2double(g);
-    PSF = fspecial('motion', 1.02, 90);
-    noise_var = 0.0001;
-    estimated_nsr = noise_var / var(I(:));
-    g = deconvwnr(g, PSF, estimated_nsr);
-    
-    g = imresize(g, 1.6);
+    % -- Noise Removal --
+    % g = filter2(fspecial('average',3), g)/255;
+    % g = medfilt2(g);
 end
 
 
